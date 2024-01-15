be669146010c01186f2acf66ed8c438cdc3043f6
DmitrijLosev <dmitrijslosevs@inbox.lv>
2024-01-15 19:27:28 +0200

    browser-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,375 +1,50 @@
-import React, {useState} from "react";
-import "./App.css";
-import {InputWithButton} from "./components/InputWithButton";
-import {Input} from "./components/Input";
-import {Button} from "./components/Button";
-// import {Task} from "./components/Task/Task";
+import React, {useState} from 'react';
+import styles from "./components/Site.module.css";
+import {NavLink, Route, Routes,Navigate} from "react-router-dom";
+import {PageOne} from "./components/pages/PageOne";
+import {PageTwo} from "./components/pages/PageTwo";
+import {PageThree} from "./components/pages/PageThree";
+import {Error404} from "./components/pages/Error404";
+import { S } from './components/_styles';
 
 
-const App = () => {
-    let [messages, setMessage] = useState([
-        {message: "message1"},
-        {message: "message2"},
-        {message: "message3"}])
 
-    let [currentMessage, setCurrentMessage] = useState<string>("")
-
-    // const setNewMessage = (newMessage: string) => {
-    //     setMessage([{message: newMessage},...messages])
-    // }
-    const setCurrentNewMessage = (currentNewMessage: string) => {
-        setCurrentMessage(currentNewMessage);
-    }
-    const onClickButtonHandler = () => {
-        setMessage([{message: currentMessage}, ...messages])
-        setCurrentMessage("")
-    }
-
-    return (
-        <div className={"App"}>
-            {/*<InputWithButton setNewMessage={setNewMessage}/>*/}
-            <Input setCurrentNewMessage={setCurrentNewMessage} currentMessage={currentMessage}/>
-            <Button name="+" onClickButtonHandler={onClickButtonHandler}/>
-            {messages.map((m, index) => <div key={index}>{m.message}</div>)}
-        </div>
-    );
-};
-
-
-export default App;
-
-
-// Hi Guys!
-// Let's reinforce our current session!
-// -You have 2 arrays. You should create a new component TASKS, where you will render these arrays.
-// -Don't forget to assign types to our data.
-
-/*
-export type DataType= {
-    title:string
-    tasks:{taskId:number, title:string,isDone:boolean}[]
-    students:string[]
-}
+const PATH = {
+    PAGE1:"/page1",
+    PAGE2:"/page2",
+    PAGE3:"/page3",
+    Error404:"/error404"
+} as const
 
 function App() {
-    const data1 = {
-        title: "What to do",
-        tasks: [
-            {taskId: 1, title: "HTML&CSS2", isDone: true},
-            {taskId: 2, title: "JS2", isDone: true}
-        ],
-        students: [
-            'Jago Wormald1',
-            'Saul Milne2',
-            'Aariz Hester3',
-            'Dion Reeve4',
-            'Anisa Ortega5',
-            'Blade Cisneros6',
-            'Malaikah Phelps7',
-            'Zeeshan Gallagher8',
-            'Isobella Vo9',
-            'Rizwan Mathis10',
-            'Menaal Leach11',
-            'Kian Walton12',
-            'Orion Lamb13',
-            'Faizah Huynh14',
-            'Crystal Vaughan15',
-            'Vivien Hickman16',
-            'Stuart Lu17',
-            'Karol Davison18',
-            'Dario Burns19',
-            'Chloe Rich20',
-            'Martyna Felix',
-            'Nida Glass',
-            'Maeve Miles',
-            'Hasnain Puckett',
-            'Ayman Cano',
-            'Safwan Perry',
-            'Fox Kelly',
-            'Louise Barlow',
-            'Malaki Mcgill',
-            'Leanna Cline',
-            'Willard Hodge',
-            'Amelia Dorsey',
-            'Kiah Porter',
-            'Jeanne Daly',
-            'Mohsin Armstrong',
-            'Laurie Rangel',
-            'Princess Tierney',
-            'Kasim Kendall',
-            'Darryl Cope',
-            'Elysha Ray',
-            'Liyana Harris',
-            'Kashif Blackburn',
-            'Atif Zimmerman',
-            'Sila Hartley',
-            'Ralphie Hebert',
-        ]
-    }
-    const data2 =   {
-        title: "What to learn",
-        tasks: [
-            {taskId: 1, title: "HTML&CSS", isDone: true},
-            {taskId: 2, title: "JS", isDone: true}
-        ],
-        students: [
-            'Rick Kane',
-            'Finnlay Bentley',
-            'Samia North',
-            'Isaac Morton',
-            'Lily-Ann Clifford',
-            'Thalia Park',
-            'Sapphire Cruz',
-            'Cieran Vazquez',
-            'Anya Estes',
-            'Dominika Field',
-            'Rosanna Chung',
-            'Safiyah Davey',
-            'Ryley Beasley',
-            'Kalvin Trejo',
-            'Evie-Mae Farrell',
-            'Juliet Valencia',
-            'Astrid Austin',
-            'Lyle Montgomery',
-            'Nisha Mora',
-            'Kylie Callaghan',
-            'Star Wilks',
-            'Marissa Colley',
-            'Asa Fuller',
-            'Leigh Kemp',
-            'Avleen Dawson',
-            'Sammy Bonilla',
-            'Acacia Becker',
-            'Coral Shepherd',
-            'Melina Molina',
-            'Kiran Bailey',
-            'Clara Escobar',
-            'Alexandru Horn',
-            'Brandon-Lee Mercado',
-            'Elouise Weston',
-            'King Long',
-            'Kerri Searle',
-            'Kanye Hamer',
-            'Elwood Benitez',
-            'Mikail Whitaker',
-            'Bobby Hardy',
-            'Talha Ferry',
-            'Priscilla Landry',
-            'Olivia-Grace Cain',
-            'Kiaan Wallace',
-            'Wesley Padilla90',
-            'Ella-Grace Wooten91',
-            'Kaif Molloy92',
-            'Kamal Broadhurst93',
-            'Bianca Ferrell94',
-            'Micheal Talbot95',
-        ]
-    }
 
     return (
-        <div className="App">
-            <Task data={data1}/>
-            <Task data={data2}/>
+        <div>
+            <S.Header><h1>HEADER</h1></S.Header>
+            <S.Body>
+                <S.NavWrapper>
+                    <ul>
+                        <li><NavLink to={PATH.PAGE1}>Page1</NavLink></li>
+                        <li><NavLink to={PATH.PAGE2}>Page2</NavLink></li>
+                        <li><NavLink to={PATH.PAGE3}>Page3</NavLink></li>
+                    </ul>
+                </S.NavWrapper>
+                <S.Content>
+                    <Routes>
+                        <Route path={"/"} element={<Navigate to={PATH.PAGE1} />}/>
+                        <Route path={PATH.PAGE1} element={<PageOne />}/>
+                        <Route path={PATH.PAGE2} element={<PageTwo />}/>
+                        <Route path={PATH.PAGE3} element={<PageThree />}/>
+                        <Route path={"/*"} element={<Error404 />}/>
+                       {/* <Route path={PATH.Error404} element={<Error404 />}/>
+                        <Route path={"/*"} element={<Navigate to={"/error404"}/>}/>*/}
+                    </Routes>
+                </S.Content>
+            </S.Body>
+            <S.Footer>abibas 2023</S.Footer>
         </div>
     );
 }
 
+
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
-
-
-//--------------------------------------------------------------
-// import React from 'react';
-// import './App.css';
-// import {Tasks} from "./Tasks";
-//
-// export type DataType = {
-//     title: string
-//     tasks: Array<TasksType>
-//     students: Array<string>
-// }
-// export type TasksType = {
-//     taskId: number
-//     title: string
-//     isDone: boolean
-// }
-//
-//
-// function App() {
-//     const data1= {
-//         title: "What to do",
-//         tasks: [
-//             {taskId: 1, title: "HTML&CSS2", isDone: true},
-//             {taskId: 2, title: "JS2", isDone: true}
-//         ],
-//         students: [
-//             'Jago Wormald1',
-//             'Saul Milne2',
-//             'Aariz Hester3',
-//             'Dion Reeve4',
-//             'Anisa Ortega5',
-//             'Blade Cisneros6',
-//             'Malaikah Phelps7',
-//             'Zeeshan Gallagher8',
-//             'Isobella Vo9',
-//             'Rizwan Mathis10',
-//             'Menaal Leach11',
-//             'Kian Walton12',
-//             'Orion Lamb13',
-//             'Faizah Huynh14',
-//             'Crystal Vaughan15',
-//             'Vivien Hickman16',
-//             'Stuart Lu17',
-//             'Karol Davison18',
-//             'Dario Burns19',
-//             'Chloe Rich20',
-//             'Martyna Felix',
-//             'Nida Glass',
-//             'Maeve Miles',
-//             'Hasnain Puckett',
-//             'Ayman Cano',
-//             'Safwan Perry',
-//             'Fox Kelly',
-//             'Louise Barlow',
-//             'Malaki Mcgill',
-//             'Leanna Cline',
-//             'Willard Hodge',
-//             'Amelia Dorsey',
-//             'Kiah Porter',
-//             'Jeanne Daly',
-//             'Mohsin Armstrong',
-//             'Laurie Rangel',
-//             'Princess Tierney',
-//             'Kasim Kendall',
-//             'Darryl Cope',
-//             'Elysha Ray',
-//             'Liyana Harris',
-//             'Kashif Blackburn',
-//             'Atif Zimmerman',
-//             'Sila Hartley',
-//             'Ralphie Hebert',
-//         ]
-//     }
-//     const data2 = {
-//         title: "What to learn",
-//         tasks: [
-//             {taskId: 1, title: "HTML&CSS", isDone: true},
-//             {taskId: 2, title: "JS", isDone: true}
-//         ],
-//         students: [
-//             'Rick Kane',
-//             'Finnlay Bentley',
-//             'Samia North',
-//             'Isaac Morton',
-//             'Lily-Ann Clifford',
-//             'Thalia Park',
-//             'Sapphire Cruz',
-//             'Cieran Vazquez',
-//             'Anya Estes',
-//             'Dominika Field',
-//             'Rosanna Chung',
-//             'Safiyah Davey',
-//             'Ryley Beasley',
-//             'Kalvin Trejo',
-//             'Evie-Mae Farrell',
-//             'Juliet Valencia',
-//             'Astrid Austin',
-//             'Lyle Montgomery',
-//             'Nisha Mora',
-//             'Kylie Callaghan',
-//             'Star Wilks',
-//             'Marissa Colley',
-//             'Asa Fuller',
-//             'Leigh Kemp',
-//             'Avleen Dawson',
-//             'Sammy Bonilla',
-//             'Acacia Becker',
-//             'Coral Shepherd',
-//             'Melina Molina',
-//             'Kiran Bailey',
-//             'Clara Escobar',
-//             'Alexandru Horn',
-//             'Brandon-Lee Mercado',
-//             'Elouise Weston',
-//             'King Long',
-//             'Kerri Searle',
-//             'Kanye Hamer',
-//             'Elwood Benitez',
-//             'Mikail Whitaker',
-//             'Bobby Hardy',
-//             'Talha Ferry',
-//             'Priscilla Landry',
-//             'Olivia-Grace Cain',
-//             'Kiaan Wallace',
-//             'Wesley Padilla90',
-//             'Ella-Grace Wooten91',
-//             'Kaif Molloy92',
-//             'Kamal Broadhurst93',
-//             'Bianca Ferrell94',
-//             'Micheal Talbot95',
-//         ]
-//     }
-//
-//     return (
-//         <div className="App">
-//             <Tasks data={data1}/>
-//             <Tasks data={data2}/>
-//         </div>
-//     );
-// }
-//
-// export default App;
-
-// import React from 'react';
-// import {DataType} from "./App";
-//
-// type  TasksPropsType = {
-//     data: DataType
-// }
-//
-// export const Tasks = (props: TasksPropsType) => {
-//     return (
-//         <div>
-//             <h1>{props.data.title}</h1>
-//             <ul>
-//                 {props.data.tasks.map(el => {
-//                     return (
-//                         <li>
-//                             <span>{el.taskId}</span>
-//                             <span>{el.title}</span>
-//                             <span>{el.isDone}</span>
-//                         </li>
-//                     )
-//                 })}
-//             </ul>
-//
-//             <ul>
-//                 {props.data.students.map(el => {
-//                     return (
-//                         <li>{el}</li>
-//                     )
-//                 })}
-//             </ul>
-//         </div>
-//     );
-// };
-
-//--------------------------------------------------------------
--- src/components/Button.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const Button:React.FC<{name:string; onClickButtonHandler:()=>void}> = ({name, onClickButtonHandler}) => {
-    return (
-        <button onClick={onClickButtonHandler}>{name}</button>
-    );
-};
-
